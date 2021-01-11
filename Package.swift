1f5a40d630a08bf718caf963ca7b106334193b2e
moonl1ght <ixav1@icloud.com>
2021-01-11 19:19:29 +0300

    added thread extension

--- Package.swift
+++ Package.swift
@@ -5,24 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "LightUtils",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LightUtils",
-            targets: ["LightUtils"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            type: .static,
+            targets: ["LightUtils"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LightUtils",
             dependencies: []),
         .testTarget(
             name: "LightUtilsTests",
-            dependencies: ["LightUtils"]),
-    ]
+            dependencies: ["LightUtils"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
